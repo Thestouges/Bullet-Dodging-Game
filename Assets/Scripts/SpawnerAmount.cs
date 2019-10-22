a9101c25cf753755e6a472e445f6962f82e525fc
Thestouges <thestouges@gmail.com>
2019-10-22 17:19:29 -0400

    Saved to game exe

---
+++ Assets/Scripts/SpawnerAmount.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SpawnerAmount : MonoBehaviour
+{
+    public int totalSpawners;
+    public List<GameObject> spawners;
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalSpawners = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalSpawners = GameObject.FindGameObjectsWithTag("Spawner").Length;
+        GetComponent<Text>().text = "Total Spawners: " + totalSpawners;
+    }
+}
